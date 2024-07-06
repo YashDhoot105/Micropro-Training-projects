048e87bd10a51444d58ede3a0e866b4594832448
YashDhoot105 <yashdhoot105@gmail.com>
2024-07-06 17:24:49 +0530

    added two components login and register and implemented functionality of routing in angular

--- Form-Using-Angular/src/app/app.component.ts
+++ Form-Using-Angular/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Form-Using-Angular';
 }
--- Form-Using-Angular/src/app/app.routes.ts
+++ Form-Using-Angular/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component:LoginComponent },
+    { path: 'register', component:RegisterComponent },
+    { path: 'home', component:HomeComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
