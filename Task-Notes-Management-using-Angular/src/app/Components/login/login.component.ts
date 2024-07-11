1ef246b821392214c4ba9d36f6c1885ceae0f650
YashDhoot105 <yashdhoot105@gmail.com>
2024-07-11 18:27:05 +0530

    minor changes

--- Task-Notes-Management-using-Angular/src/app/Components/login/login.component.ts
+++ Task-Notes-Management-using-Angular/src/app/Components/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,29 +13,34 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
+export class LoginComponent implements OnInit {
+  loginform: FormGroup;
+  isloggingin: boolean = false;
 
-export class LoginComponent implements OnInit{
-
-  // loginform: FormGroup;
-  isloggingin:boolean = false;
-
-  // constructor(){
-    loginform: FormGroup = new FormGroup ({
-      login_username : new FormControl('',[Validators.required,Validators.minLength(2), Validators.maxLength(25)]),
-      login_password : new FormControl('', [Validators.required,Validators.minLength(10)]),
+  constructor() {
+    this.loginform = new FormGroup({
+      login_username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]),
+      login_password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
     });
-  // }
-
-  
+  }
 
   login() {
-    console.log(this.loginform.value);
-   }
-
-  ngOnInit(): void { }
+    if (this.loginform.valid) {
+      console.log('Form submitted:', this.loginform.value);
+      // Implement login logic here
+    } else {
+      console.error('Form is invalid');
+      // Optionally handle invalid form state
+    }
+  }
+  ngOnInit(): void {}
 }
-
-
-
